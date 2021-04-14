c840af3903f43f8708f2c7cf28cd1bf4a4548a04
AlSakharoB <71816331+AlSakharoB@users.noreply.github.com>
2021-04-14 11:37:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from sdl2 import SDL_RenderDrawPoint
 import maze
 import sdl2.ext
 sdl2.ext.init()
@@ -8,38 +9,6 @@ GREY = sdl2.ext.Color(165, 165, 165)
 RED = sdl2.ext.Color(255, 0, 0)
 GREEN = sdl2.ext.Color(0, 255, 0)
 BLACK = sdl2.ext.Color(0, 0, 0)
-
-
-class MovementSystem(sdl2.ext.Applicator):
-    def __init__(self, minx, miny, maxx, maxy):
-        super(MovementSystem, self).__init__()
-        self.componenttypes = Speed, sdl2.ext.Sprite
-        self.minx = minx
-        self.miny = miny
-        self.maxx = maxx
-        self.maxy = maxy
-
-    def process(self, world, componentsets):
-        for velocity, sprite in componentsets:
-            swidth, sheight = sprite.size
-            sprite.x += velocity.vx
-            sprite.y += velocity.vy
-            sprite.x = max(self.minx, sprite.x)
-            sprite.y = max(self.miny, sprite.y)
-
-            pmaxx = sprite.x + swidth
-            pmaxy = sprite.y + sheight
-            if pmaxx > self.maxx:
-                sprite.x = self.maxx - swidth
-            if pmaxy > self.maxy:
-                sprite.y = self.maxy - sheight
-
-
-class Speed(object):
-    def __init__(self):
-        super(Speed, self).__init__()
-        self.vx = 0
-        self.vy = 0
 
 
 class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
@@ -55,7 +24,6 @@ class Entity(sdl2.ext.Entity):
     def __init__(self, world, sprite, posx=0, posy=0):
         self.sprite = sprite
         self.sprite.position = posx, posy
-        self.speed = Speed()
 
 
 class DrawLines(sdl2.ext.Entity):
@@ -69,25 +37,25 @@ def run():
     window.show()
 
     spriterenderer = SoftwareRenderer(window)
-    movement = MovementSystem(70, 70, 730, 730)
     factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-
-    sp_paddle = factory.from_color(BLACK, size=(24, 24))
+    sp_paddle = factory.from_color(BLACK, size=(25, 25))
     start = factory.from_color(GREEN, size=(57, 57))
     finish = factory.from_color(RED, size=(57, 57))
 
     maze_ = maze.RandomMaze()
     world = sdl2.ext.World()
     world.add_system(spriterenderer)
-    world.add_system(movement)
 
     bg = factory.from_color(GREY, size=(680, 680))
     bg_ = DrawLines(world, bg, 60, 60)
-
+    borders = [[] for i in range(58)]
+    i = 0
     for line in maze_:
         line1 = factory.from_color(WHITE, size=(line[2], line[3]))
         line_ = DrawLines(world, line1, line[0], line[1])
-
+        for j in range(4):
+            borders[i].append(int(line_.sprite.area[j]))
+        i += 1
     start_ = DrawLines(world, start, 70, 70)
     finish_ = DrawLines(world, finish, 673, 673)
     player = Entity(world, sp_paddle, 86, 86)
@@ -99,20 +67,54 @@ def run():
                 running = False
                 break
             if event.type == sdl2.SDL_KEYDOWN:
-                if event.key.keysym.sym == sdl2.SDLK_w:
-                    player.speed.vy = -1
+                left, top, right, bottom = player.sprite.area
+                if event.key.keysym.sym == sdl2.SDLK_w or event.key.keysym.sym == sdl2.SDLK_s:
+                    for each in borders:
+                        if each[0] < int(left) and each[2] > int(right) and each[1] < int(top) and each[3] > int(bottom):
+                            coll_area = each
+                            break
+                    speed = 6
+                    if top - coll_area[1] < 6:
+                        speed = top - coll_area[1] - 1
+                    if top - speed - 1 >= coll_area[1] and event.key.keysym.sym == sdl2.SDLK_w:
+                        player = Entity(world, sp_paddle, left, top - speed)
+                    else:
+                        player = Entity(world, sp_paddle, left, top)
                 if event.key.keysym.sym == sdl2.SDLK_s:
-                    player.speed.vy = 1
+                    for each in borders:
+                        if each[0] < int(left) and each[2] > int(right) and each[1] < int(top) and each[3] > int(bottom):
+                            coll_area = each
+                            break
+                    speed = 6
+                    if coll_area[3] - bottom < 6:
+                        speed = coll_area[3] - bottom - 1
+                    if bottom + speed + 1 <= coll_area[3] and event.key.keysym.sym == sdl2.SDLK_s:
+                        player = Entity(world, sp_paddle, left, top + speed)
+                    else:
+                        player = Entity(world, sp_paddle, left, top)
                 if event.key.keysym.sym == sdl2.SDLK_a:
-                    player.speed.vx = -1
+                    for each in borders:
+                        if each[0] < int(left) and each[2] > int(right) and each[1] < int(top) and each[3] > int(bottom):
+                            coll_area = each
+                    speed = 6
+                    if left - coll_area[0] < 6:
+                        speed = left - coll_area[0] - 1
+                    if left - speed - 1 >= coll_area[0] and event.key.keysym.sym == sdl2.SDLK_a:
+                        player = Entity(world, sp_paddle, left - speed, top)
+                    else:
+                        player = Entity(world, sp_paddle, left, top)
                 if event.key.keysym.sym == sdl2.SDLK_d:
-                    player.speed.vx = 1
-            else:
-                if event.key.keysym.sym in (sdl2.SDLK_w, sdl2.SDLK_s):
-                    player.speed.vy = 0
-                elif event.key.keysym.sym in (sdl2.SDLK_a, sdl2.SDLK_d):
-                    player.speed.vx = 0
-        sdl2.SDL_Delay(10)
+                    for each in borders:
+                        if each[0] < int(left) and each[2] > int(right) and each[1] < int(top) and each[3] > int(bottom):
+                            coll_area = each
+                    speed = 6
+                    if coll_area[2] - right < 6:
+                        speed = coll_area[2] - right - 1
+                    if right + speed + 1 <= coll_area[2] and event.key.keysym.sym == sdl2.SDLK_d:
+                        player = Entity(world, sp_paddle, left + speed, top)
+                    else:
+                        player = Entity(world, sp_paddle, left, top)
+        sdl2.SDL_Delay(1)
         world.process()
     return 0
 
